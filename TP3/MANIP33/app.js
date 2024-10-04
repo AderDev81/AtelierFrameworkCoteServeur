d94e31565e5993bf14145de94dc7ab614438d104
AderDev81 <75450121+AderDev81@users.noreply.github.com>
2024-10-04 23:57:31 +0100

    Add files via upload

---
+++ TP3/MANIP33/app.js
@@ -0,0 +1,9 @@
+const d = new Date();
+console.log('l date d aujourd\'hui est '+d);
+const year = d.getFullYear();
+console.log(year);
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
